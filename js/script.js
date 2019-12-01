7ba5dd433aee82a8e332eea09812be9f91518a4e
wasiliev96 <wasiliev.cloud@gmail.com>
2019-12-01 13:53:01 +0200

    update to css grid. footer created

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// let gallery = document.getElementById( 'gallery' );
+
+// window.addEventListener( 'wheel', function ( e )
+// {
+
+//     if ( e.deltaY > 0 ) gallery.scrollLeft += 10;
+//     else gallery.scrollLeft -= 10;
+// } );
